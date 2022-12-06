c1ff7a9610ba8850a08f977c240ebea719dda5b4
ElijahColwill <elijah.c035@gmail.com>
2022-12-06 11:07:07 -0500

    Add index on Rider/Driver for email

--- website/models.py
+++ website/models.py
@@ -9,7 +9,7 @@ class Rider(db.Model, UserMixin):
     __table_args__ = {'extend_existing': True}
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
+    email = db.Column(db.String(150), unique=True, index=True)
     password = db.Column(db.String(150))
     name = db.Column(db.String(150))
     DOB = db.Column(db.String(10))
@@ -21,7 +21,7 @@ class Driver(db.Model, UserMixin):
     __table_args__ = {'extend_existing': True}
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
+    email = db.Column(db.String(150), unique=True, index=True)
     password = db.Column(db.String(150))
     name = db.Column(db.String(150))
     DOB = db.Column(db.String(10))
